21bdc3957f455a0609a84495bedb5076e70ccb1a
Hiroki Teranishi <teranishihiroki@gmail.com>
2017-05-03 21:29:40 +0900

    refactor.

--- coordparser/utility.h
+++ coordparser/utility.h
@@ -3,12 +3,17 @@
 // Copyright (c) 2017 Hiroki Teranishi. All rights reserved.
 //
 
-#ifndef COORDPARSER_UTILITY_H
-#define COORDPARSER_UTILITY_H
+#ifndef COORDPARSER_UTILITY_H_
+#define COORDPARSER_UTILITY_H_
 
 #include <boost/format.hpp>
-#include <sstream>
+
 #include <fstream>
+#include <functional>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
 
 #define DEFAULT_COPY_AND_ASSIGN(TypeName) \
   TypeName(const TypeName&) = default; \
@@ -40,19 +45,20 @@ namespace string {
 
 namespace internal {
 
-static std::string format(boost::format& fmt) {
+static std::string format(const boost::format& fmt) {
     std::stringstream stream;
     stream << fmt;
     return stream.str();
 }
 
 template<typename T, typename... Args>
-static std::string format(boost::format& fmt, const T& arg, Args&& ... args) {
+static std::string format(boost::format& fmt,  // NOLINT(runtime/references)
+                          const T& arg, Args&& ... args) {
     fmt = fmt % arg;
     return format(fmt, std::forward<Args>(args)...);
 }
 
-}
+}  // namespace internal
 
 template<typename... Args>
 std::string format(const char* fmt, Args&& ... args) {
@@ -60,22 +66,29 @@ std::string format(const char* fmt, Args&& ... args) {
     return internal::format(boost_format, std::forward<Args>(args)...);
 }
 
-static inline void ltrim(std::string& s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
-                                    std::not1(std::ptr_fun<int, int>(std::isspace))));
+static inline void ltrim(std::string& s) {  // NOLINT(runtime/references)
+    s.erase(s.begin(),
+            std::find_if(
+                s.begin(),
+                s.end(),
+                std::not1(std::ptr_fun<int, int>(std::isspace))));
 }
 
-static inline void rtrim(std::string& s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(),
-                         std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+static inline void rtrim(std::string& s) {  // NOLINT(runtime/references)
+    s.erase(std::find_if(
+                s.rbegin(),
+                s.rend(),
+                std::not1(std::ptr_fun<int, int>(std::isspace))).base(),
+            s.end());
 }
 
-static inline void trim(std::string& s) {
+static inline void trim(std::string& s) {  // NOLINT(runtime/references)
     ltrim(s);
     rtrim(s);
 }
 
-static inline std::vector<std::string> split(const std::string& string, char delimiter) {
+static inline std::vector<std::string> split(const std::string& string,
+                                             char delimiter) {
     std::istringstream stream(string);
     std::string field;
     std::vector<std::string> splits;
@@ -85,7 +98,7 @@ static inline std::vector<std::string> split(const std::string& string, char del
     return splits;
 }
 
-}
+}  // namespace string
 
 namespace vector {
 
@@ -102,7 +115,7 @@ static inline std::string join(const std::vector<T>& vector, char delimiter) {
     return ss.str();
 }
 
-}
+}  // namespace vector
 
 namespace file {
 
@@ -111,8 +124,8 @@ static inline void write(const char* path, const char* data) {
     outFile << data << std::endl;
 }
 
-}
+}  // namespace file
 
-}
+}  // namespace utility
 
-#endif //COORDPARSER_UTILITY_H
+#endif  // COORDPARSER_UTILITY_H_

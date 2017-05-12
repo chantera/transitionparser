9a692ca6a47a90b02c918a5aa02855d4b6717862
Hiroki Teranishi <teranishihiroki@gmail.com>
2017-05-12 17:41:48 +0900

    setup googletest.

---
+++ test/main.cc
@@ -0,0 +1,11 @@
+//
+// Created by h.teranishi <teranishihiroki@gmail.com>
+// Copyright (c) 2017 Hiroki Teranishi. All rights reserved.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
